0eb41a46a36a3740ce5492f2fe1991a0fbca8a22
Jonatan Antoni <jonatan.antoni@arm.com>
2023-07-31 14:44:15 +0200

    Enhance bash wrapper with iris flags.

--- bin/fvp.sh
+++ bin/fvp.sh
@@ -3,6 +3,18 @@
 MODEL=$(basename "$0")
 ARMLM_CACHED_LICENSES_LOCATION="${ARMLM_CACHED_LICENSES_LOCATION:-$HOME/.armlm}"
 
-docker run --platform linux/amd64 --mount "type=bind,src=$ARMLM_CACHED_LICENSES_LOCATION,dst=/home/$(whoami)/.armlm" --mount "type=bind,src=$HOME,dst=$HOME" -w "$(pwd)" fvp "${MODEL}" "$@"
+FLAGS=("$@")
+PORTS=()
+if [[ "${FLAGS[*]}" =~ "-I" || "${FLAGS[*]}" =~ "--iris-server" ]]; then
+    PORTS+=("-p" "7100:7100")
+    if [[ ! "${FLAGS[*]}" =~ "--print-port-number" && ! "${FLAGS[*]}" =~ "-p" ]]; then
+        FLAGS+=("--print-port-number")
+    fi
+    if [[ ! "${FLAGS[*]}" =~ "--iris-allow-remote" && ! "${FLAGS[*]}" =~ "-A" ]]; then
+        FLAGS+=("--iris-allow-remote")
+    fi
+fi
+
+docker run --platform linux/amd64 "${PORTS[@]}" --mount "type=bind,src=$ARMLM_CACHED_LICENSES_LOCATION,dst=/home/$(whoami)/.armlm" --mount "type=bind,src=$HOME,dst=$HOME" -w "$(pwd)" fvp "${MODEL}" "${FLAGS[@]}"
 
 exit
